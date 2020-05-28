4bf4530131be427decc862213b3fbb49991da465
ZhangYDevelop <2389311433@qq.com>
2020-05-28 13:18:17 +0800

    spring cloud 整合dubbo nacos

--- user-service/user-service-nacos-cloud/src/main/java/com/zy/user/service/nacos/cloud/UserServiceNacosCloud.java
+++ user-service/user-service-nacos-cloud/src/main/java/com/zy/user/service/nacos/cloud/UserServiceNacosCloud.java
@@ -1,12 +1,13 @@
 package com.zy.user.service.nacos.cloud;
 
 
-import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+//import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication()
-@EnableDubbo
+@SpringBootApplication
+@EnableDiscoveryClient
 public class UserServiceNacosCloud {
 
     public static void main(String[] args) {
