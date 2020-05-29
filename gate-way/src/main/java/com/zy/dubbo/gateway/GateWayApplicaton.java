8228b4969ad3c165ba7880535a978c18080cc021
ZhangYDevelop <2389311433@qq.com>
2020-05-29 14:31:20 +0800

    调整

--- gate-way/src/main/java/com/zy/dubbo/gateway/GateWayApplicaton.java
+++ gate-way/src/main/java/com/zy/dubbo/gateway/GateWayApplicaton.java
@@ -9,9 +9,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableDubbo
-public class DubboGateWayApplicaton {
+public class GateWayApplicaton {
 
     public static void main(String[] args) {
-        SpringApplication.run(DubboGateWayApplicaton.class, args);
+        SpringApplication.run(GateWayApplicaton.class, args);
     }
 }
